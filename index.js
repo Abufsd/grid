d343afc36fd5c35607335fc759eb719f0388d69d
Abufsd <abarna1806@gmail.com>
2025-02-06 20:09:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './mystyles.css';
+function Conditional()
+{
+    return(<div className='blue'>
+        <h1>Conditional Statement</h1>
+    </div>)
+}
+export default Conditional;
